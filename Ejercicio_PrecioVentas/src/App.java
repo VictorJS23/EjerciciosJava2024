269884dba50f0eabed42d5637cbdddbe383c1f3d
VictorJS23 <vb329708@gmail.com>
2024-09-29 21:54:16 +0200

    testing an excersice

--- Ejercicio_PrecioVentas/src/App.java
+++ Ejercicio_PrecioVentas/src/App.java
@@ -1,24 +1,54 @@
 import java.util.Scanner;
-
-/*
- * "Calcular el precio de venta de un producto, si se sabe que este debe tener una ganancia del 30% en relacion al precio de compra"
- */
+import java.util.Locale; // Esto funciona para forzar el uso del . en los decimales
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        double precioVenta, precioCompra;
+        /*
+         * "Calcular el precio de venta de un producto, si se sabe que este debe tener una ganancia del 30% en relacion al precio de compra"
+         */
 
-        Scanner entrada = new Scanner(System.in);
+        // double precioVenta, precioCompra;
 
-        System.out.println("Ingrese el precio del producto: ");
-        precioCompra = entrada.nextFloat();
+        // Scanner entrada = new Scanner(System.in);
 
-        precioVenta = precioCompra + (precioCompra * 0.30);
+        // System.out.println("Ingrese el precio del producto: ");
+        // precioCompra = entrada.nextFloat();
 
-        System.out.println("El precio de venta del producto es de: " + precioVenta);
+        // precioVenta = precioCompra + (precioCompra * 0.30);
 
-        entrada.close();
+        // System.out.println("El precio de venta del producto es de: " + precioVenta);
+
+        // entrada.close();
+
+        Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US);
+
+        System.out.print("Ingrese el numero de alumnos en su clase: ");
+        int numero = input.nextInt();
+        input.nextLine(); // Consumir la nueva línea
+
+        String[] alumnos = new String[numero];
+
+        for (int i = 0; i < alumnos.length; i++) {
+            System.out.print("Ingrese el nombre de su estudiante numero " + (i + 1) + " : ");
+            alumnos[i] = input.nextLine();
+        }
+
+        double[] calificaciones = new double[numero];
+        double promedio = 0;
+
+        for (int i = 0; i < calificaciones.length; i++) {
+            System.out.print("Ingrese las calificaciones de " + alumnos[i] + " :");
+            calificaciones[i] = input.nextDouble();
+            promedio += calificaciones[i];
+        }
+
+        promedio /= numero;
+
+        System.out.print("El promedio de las calificaciones de todo el grupo de alumnos es de: " + promedio);
+
+        input.close();
 
     }
 }
